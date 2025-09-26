93df8b3f49ed887b404930228b783e65d2feada2
BeSuper123 <bugochukwu17@gmail.com>
2025-09-26 12:14:06 +0100

    changed hello.py

--- hello.py
+++ hello.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>Hello, World, I am a flask app!</p> <br> <a href="/about">About Section</a>'
+	return '<p>Hello, World, I am a flask app!</p> <br> <a href="/about">About Section</a> <br> <a href="/contact">Contact Us</a>'
 
 @app.route('/about')
 def about_sec():
-	return 'The following link is a link to the flask explanation page -> <a href="https://flask.palletsprojects.com">Link</a>'
+	return '<p>The following link is a link to the flask explanation page -></p> <a href="https://flask.palletsprojects.com">Link</a>'
+
+@app.contact('/contact')
+def contact_sec():
+	return '<p>My Name: Blessing Ugochukwu <br> My Email: bugochukwu17@gmail.com</p>'
+	
