e9b688cd2977340492a028ad4f66eaeab9fe8c67
Blessing Ugochukwu <49164980+BeSuper123@users.noreply.github.com>
2025-10-03 12:52:57 +0100

    changed hello.py

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ def say_hello():
 def about_sec():
 	return '<p>The following link is a link to the flask explanation page -></p> <a href="https://flask.palletsprojects.com">Link to flask</a> <br><br><a href="https://python.org">link to python</a>'''
 
-@app.contact('/contact')
+@app.route('/contact')
 def contact_sec():
 	return '<p>My Name: Blessing Ugochukwu <br> My Email: Bugochukwu17@gmail.com</p>'
 	
