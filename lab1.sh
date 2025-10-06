200e19314181d08a915cd4e0f1f72b2c0abe1124
BeSuper123 <bugochukwu17@gmail.com>
2025-10-06 16:36:27 +0100

    changed some stuff to do woth vagrant

--- lab1.sh
+++ lab1.sh
@@ -2,7 +2,8 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y nano vim python-is-python3 python3-venv python3-pip
-python -m venv .my_venv
-source .my_venv/bin/activate
+python -m venv /home/vagrant/.my_venv
+source /home/vagrant/.my_venv/bin/activate
 pip install flask
-flask --app hello run --host=0.0.0.0
+flask --app /vagrant/hello.py run --host=0.0.0.0
+ 
